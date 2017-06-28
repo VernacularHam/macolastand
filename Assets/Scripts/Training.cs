6f6035c422ca94c7c7f7eb8c1f4aa2ac49767d07
Stephanie Muhammad <srmuhammad70@gmail.com>
2017-06-28 12:22:20 -0400

    Added Marketing and Training Classes. Added to model

---
+++ Assets/Scripts/Training.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Training
+{
+    public Training()
+    {
+        _costMultiplier = 1.2;
+        _cost = 5.00;
+        _level = 1;
+}
+
+    private double _costMultiplier;
+    private List<string> _lockedResearch;
+
+    /// <summary>
+    /// Cost in units of work.
+    /// </summary>
+    private double _cost;
+    public double Cost
+    {
+        get { return _cost; }
+        //set
+        //{
+        //    _cost = value;
+        //}
+    }
+
+    private int _level;
+    public int Level
+    {
+        get { return _level; }
+        //set
+        //{
+        //    _level = value;
+        //}
+    }
+
+    public void Update()
+    {
+        _level++;
+        _cost *= _costMultiplier;
+    }
+
+}
